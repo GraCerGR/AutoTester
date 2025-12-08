dd45583a1eea82a6c596fe23b2d5d1400d0cf47f
Григорий <ctrhtnysq.afqk@gmail.com>
2025-12-08 15:19:48 +0700

    Разделил мэйн на раннер и эксекьютор

--- classes/attempt.go
+++ classes/attempt.go
@@ -7,19 +7,19 @@ import (
 )
 
 var Attempt struct {
-	id                        uuid.UUID
-	created_at                time.Time
-	git_student_url           string
-	git_site_url              string
-	task_id                   uuid.UUID
-	task_name                 string
-	programming_language_id   uuid.UUID
-	programming_language_name string
-	testing_verdict           TestVerdict
-	postmoderation            TestVerdict
+	id                   uuid.UUID
+	created_at           time.Time
+	git_student_url      string
+	git_site_url         string
+	variable_with_url    string
+	task_id              uuid.UUID
+	task_name            string
+	programming_language programmingLanguage
+	testing_verdict      TestVerdict
+	postmoderation       TestVerdict
 }
 
-var ProgrammingLanguage struct {
+type programmingLanguage struct {
 	id   uuid.UUID
 	name string
 }
---
+++ runner.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	myredis "MainApp/redis"
+	selenium_grid "MainApp/seleniumgrid"
+	"MainApp/settings"
+	"context"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+)
+
+func Runner() (context.Context, *redis.Client, error) {
+
+	// ---- Runner ----
+
+	//Запуск компоуза с гридом, тестом и сайтом
+	//Запускается при запуске всего хаба
+	if err := selenium_grid.StartSeleniumGrid("./seleniumgrid"); err != nil {
+		fmt.Printf("Ошибка запуска Selenium Grid: %v\n", err)
+		return nil, nil, err
+	}
+
+	//Редис
+	ctx := context.Background()
+	redisClient, err := redisClientStart()
+	if err != nil {
+		fmt.Printf("Ошибка запуска Redis: %v\n", err)
+		return nil, nil, err
+	}
+
+	// Пометка в редисе, что все контейнеры свободны
+	for _, i := range settings.TestContainers {
+		myredis.InitOrUpdateContainer(ctx, redisClient, i, "free")
+	}
+	for _, i := range settings.SiteContainers {
+		myredis.InitOrUpdateContainer(ctx, redisClient, i, "free")
+	}
+
+	return ctx, redisClient, nil
+}
+
+
+func redisClientStart() (*redis.Client, error) {
+	cfg := myredis.Config{
+		Addr:        settings.RedisAddr,
+		Password:    settings.RedisPassword,
+		User:        "",
+		DB:          settings.RedisDB,
+		MaxRetries:  settings.RedisMaxRetries,
+		DialTimeout: settings.DialTimeout,
+		Timeout:     settings.RedisTimeout,
+	}
+
+	db, err := myredis.NewClient(context.Background(), cfg)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connected to Redis:", cfg.Addr)
+	return db, nil
+}
